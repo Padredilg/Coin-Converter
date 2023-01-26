9e591d411b8992e13b655e62aed5b1b24c65ef40
Padredilg <padredilg@knights.ucf.edu>
2023-01-25 21:33:48 -0500

    created logic to separate amount into coin quantity, and included dash separation between prompts.

--- LuizPadredi_Assignment1.cpp
+++ LuizPadredi_Assignment1.cpp
@@ -2,17 +2,7 @@
 Author: Luiz Padredi
 Date: 01/24/2023
 Assignment 1: Coin Dispenser Machine
-*/
-
-/*
-    a)  What are inputs to the program? Declare all the input values as appropriate types.
-    b)  Create a prompting message to prompt the user to input their coin denominations.
-    c)  What’s the expected output? Declare appropriate variables for to store the results.
-    d)  What’s the algorithm to solve the problem? How do you relate the inputs to the output?
-        • Make use of the arithmetic operators to solve this problem.
-        • To separate the dollars and cents use the % and / operator respectively.
-            For the above example 135 cents: 135 / 100 = 1  (dollars) and 135 % 100 = 35 (cents).
-    e)  Display the outputs in an informative manner.
+Repository: https://github.com/Padredilg/Coin-Converter
 */
 
 #include <iostream>
@@ -28,6 +18,10 @@ double collectDoubleInput();
 
 int main()
 {
+    cout.setf(ios::fixed);
+    cout.setf(ios::showpoint);
+    cout.precision(2);
+
     cout << "Hello user!"
          << endl << endl
          << "What kind of conversion are you looking to do?" << endl;
@@ -75,55 +69,56 @@ void promptForChoice(){
 This function handles the scenario when user wants to convert from an amount to a distribution of coins.
 */
 void amountToCoin(){
-    cout << "AMOUNT --> COINS"
+    cout << "================================================================================"
          << endl << endl
-         << "Please enter the dollar amount in the format '$X.XX' that you would like to receive in coins: ";
+         << "AMOUNT --> COINS"
+         << endl << endl
+         //improve this verbiage
+         << "Please enter the dollar amount in the format '$X.XX' that you would like to receive in coins." << endl
+         << "(Do not include commas or any special characters other than a period to declare cents)"
+         << endl << endl
+         << "Amount: $";
 
-    double amount = collectDoubleInput();
-    cout << "You entered: " << amount << endl << endl;
+    double dollarAmount = collectDoubleInput();
+    cout << "You entered: $" << dollarAmount << endl << endl;
 
+    if(dollarAmount > 0){
+    //    First convert the input amount into cents: $5.42 * 100
+        int centAmount = dollarAmount * 100;
+    //    First the larger coin, quarters
+    //    542 / 25 ----> 21 quarters
+    //    542 % 25 ----> 17 cents of change
+        int quarterCount = centAmount / 25;
+        centAmount = centAmount % 25;
+    //    Next comes dime.
+    //    17 / 10 ---- > 1 dime
+    //    17 % 10 -----> 7 cents of change
+        int dimeCount = centAmount / 10;
+        centAmount = centAmount % 10;
+    //    After that comes nickels
+    //    7 / 5 -----> 1
+    //    7 % 5 -------> 2
+        int nickelCount = centAmount / 5;
+        centAmount = centAmount % 5;
+    //    Finally, pennies
+    //     2 pennies are what’s left.
 
-    /*
-    In the first part, the machine asks for the dollar amount of coin change that the user
-    would need. For an input of $5.42 the machine dispenses the following:
-    - 21 quarters
-    - 1 dime
-    - 1 nickel
-    - 2 pennies
-    */
-    //obs: so while it can dispense higher coins, do it
-
-    //PART 1
-    /*
-    Write a program that would prompt the user to input some value in dollars and cents in
-    the format of $x.xx and figures out the equivalent number of coins.
-
-    First convert the input amount into cents: $x.xx * 100
-
-    Then decide on the coin designations by following the algorithm below:
-
-    $5.42 is equivalent to 542 cents.
-
-    First the larger coin, quarters
-    542 / 25 ----> 21 quarters
-    542 % 25 ----> 17 cents of change
-
-    Next comes dime.
-    17 / 10 ---- > 1 dime
-    17 % 10 -----> 7 cents of change
-
-    After that comes nickels
-    7 / 5 -----> 1
-    7 % 5 -------> 2
-    Finally, pennies
-     2 pennies are what’s left.
-
-    The next step is displaying the original dollar amount along with the coin designations and
-    their number.
-    */
+    //    The next step is displaying the original dollar amount along with the coin designations and
+    //    their number.
+        if(quarterCount > 0)
+            cout << "Quarter count = " << quarterCount << endl;
+        if(dimeCount > 0)
+            cout << "Dime count = " << dimeCount << endl;
+        if(nickelCount > 0)
+            cout << "Nickel count = " << nickelCount << endl;
+        if(centAmount > 0)
+            cout << "Penny count = " << centAmount << endl;
+    }
+    else{ //user input 0
+        cout << "Very funny. Here are your coins, oh, wait, there are none!" << endl;
+    }
 
     askForNewConversion();
-    //would you like to do a new conversion? -- Y or N -- if Y, promptForChoice
     return;
 }
 
@@ -131,7 +126,10 @@ void amountToCoin(){
 This function handles the scenario when user wants to convert from coins to a total amount.
 */
 void coinToAmount(){
-    cout << "COINS --> AMOUNT" << endl << endl;
+    cout << "================================================================================"
+         << endl << endl
+         << "COINS --> AMOUNT"
+         << endl << endl;
 
     //PART 2
     /*
@@ -146,18 +144,30 @@ void coinToAmount(){
     (Sum all, then deal with remaining cents after)
     */
 
+    /*
+    a)  What are inputs to the program? Declare all the input values as appropriate types.
+    b)  Create a prompting message to prompt the user to input their coin denominations.
+    c)  What’s the expected output? Declare appropriate variables for to store the results.
+    d)  What’s the algorithm to solve the problem? How do you relate the inputs to the output?
+        • Make use of the arithmetic operators to solve this problem.
+        • To separate the dollars and cents use the % and / operator respectively.
+            For the above example 135 cents: 135 / 100 = 1  (dollars) and 135 % 100 = 35 (cents).
+    e)  Display the outputs in an informative manner.
+    */
+
     askForNewConversion();
     //would you like to do a new conversion? -- Y or N -- if Y, promptForChoice
     return;
 }
 
-//FIXME -- Fix case when user inputs more than one char. -- create function that checks type of input and if not an int it assumes 0.
 /*
 This function verifies if the user would like to do a new conversion.
 */
 void askForNewConversion(){
 
-    cout << "Would you like to do another conversion?" << endl
+    cout << "================================================================================"
+         << endl << endl
+         << "Would you like to do another conversion?" << endl
          << "Please select the number corresponding to the service you are looking for."
          << endl << endl
          << "1 - Yes" << endl
@@ -182,7 +192,7 @@ void askForNewConversion(){
 }
 
 /*
-This function ensures that the user has input an int.
+This function ensures that the user has input a non-negative int.
 */
 int collectIntInput() {
     int choice;
@@ -196,6 +206,9 @@ int collectIntInput() {
     return choice;
 }
 
+/*
+This function ensures that the user has input a non-negative double.
+*/
 double collectDoubleInput() {
     double amount;
     cin >> amount;
